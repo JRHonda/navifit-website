2003bfc3ac9fb0c5f1e8fa645349c721e744829f
Justin Honda <justinohonda@gmail.com>
2019-10-17 00:31:23 -0400

    Updates

--- precache-manifest.8b9b7734b1bff8bfe6c1d8ec14fbc096.js
+++ precache-manifest.8b9b7734b1bff8bfe6c1d8ec14fbc096.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16017f9d8aeac8708a57c43185f79a81",
+    "revision": "c1aca946e363573cb9fb86265e8f16a0",
     "url": "/navifit-website/index.html"
   },
   {
-    "revision": "06d673185f92f6a25d68",
-    "url": "/navifit-website/static/css/main.9a880b77.chunk.css"
+    "revision": "7715461236837a1dfc39",
+    "url": "/navifit-website/static/css/main.e0185180.chunk.css"
   },
   {
     "revision": "90dce2f158cb6737bbb7",
     "url": "/navifit-website/static/js/2.bd614364.chunk.js"
   },
   {
-    "revision": "06d673185f92f6a25d68",
+    "revision": "7715461236837a1dfc39",
     "url": "/navifit-website/static/js/main.546e1791.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/navifit-website/precache-manifest.230dc4302b652af42e761a346c40ef36.js"
+  "/navifit-website/precache-manifest.8b9b7734b1bff8bfe6c1d8ec14fbc096.js"
 );
 
 self.addEventListener('message', (event) => {
